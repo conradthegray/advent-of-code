fdd3d47d8da9979f10793c5e5b5558e996249daf
Conrad Gray <hello@conradthegray.com>
2024-12-02 20:52:45 +0000

    feat(2024): Day 2

---
+++ 2024/02/solution.ts
@@ -0,0 +1,38 @@
+import { readFile } from "node:fs/promises";
+import * as path from "node:path";
+
+const filePath = path.resolve(import.meta.dirname!, "./input.txt");
+const fileContents = await readFile(filePath, { encoding: "utf8" });
+
+const reports: number[][] = fileContents
+  .trim()
+  .split("\n")
+  .map((row) => row.split(" ").map((s) => Number(s)));
+
+const isSafe = (report: number[]): boolean => {
+  const diffs = report.slice(1).map((num, i) => num - report[i]);
+  return diffs.every(
+    (diff) =>
+      Math.sign(diff) === Math.sign(diffs[0]) &&
+      Math.abs(diff) >= 1 &&
+      Math.abs(diff) <= 3
+  );
+};
+
+const result = reports
+  .map((report) => isSafe(report))
+  .reduce((acc, curr) => acc + Number(curr), 0);
+
+console.log(`Number of safe reports: ${result}`);
+
+const safeWithDampener = reports
+  .map((report) => {
+    const subReports = report
+      .map((_, i) => [...report.slice(0, i), ...report.slice(i + 1)])
+      .map((subReport) => isSafe(subReport));
+
+    return subReports.some((r) => r === true);
+  })
+  .reduce((acc, curr) => acc + Number(curr), 0);
+
+console.log(`Number of safe reports with dampener: ${safeWithDampener}`);
