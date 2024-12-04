41aeab04d515a991aec6a381b31c7debae751197
Conrad Gray <hello@conradthegray.com>
2024-12-04 13:34:41 +0000

    feat(2024): Day 3

--- 2024/03/solution.ts
+++ 2024/03/solution.ts
@@ -18,23 +18,25 @@ const sum = memory
 console.log(`Answer for part 1: ${sum}`);
 assert(sum === 175700056, "Incorrect answer for part 1");
 
+let active = true;
+
 const resultPart2 = memory
-  .map((row) => {
-    const instructions = row
+  .map((row) =>
+    row
       .matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g)
-      .map(([match, a, b]) => (b ? Number(a) * Number(b) : match));
+      .map(([match, a, b]) =>
+        !["do()", "don't()"].includes(match) ? Number(a) * Number(b) : match
+      )
+      .map((instruction) => {
+        if (instruction == "don't()") active = false;
+        else if (instruction == "do()") active = true;
+        else if (active) return Number(instruction);
 
-    let active = true;
-    let sum = 0;
-
-    for (const instr of instructions) {
-      if (active && typeof instr == "number") sum += instr;
-      if (active && instr == "don't()") active = false;
-      if (!active && instr == "do()") active = true;
-    }
-
-    return sum;
-  })
+        return 0;
+      })
+      .reduce((acc, curr) => acc + curr, 0)
+  )
   .reduce((acc, curr) => acc + curr, 0);
 
 console.log(`Answer for part 2: ${resultPart2}`);
+assert(resultPart2 === 71668682, "Incorrect answer for part 2");
